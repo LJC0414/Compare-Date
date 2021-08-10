8719d7bf2c8a842bd07a31b9ddedd9d5e755e9f1
Jiacheng Li <Jiacheng@calliaflowers.com>
2021-08-10 13:40:40 -0500

    Finished Index.js and add InputTime.json to input date

--- Index.js
+++ Index.js
@@ -1,2 +1,19 @@
-//require dayjs module
-const dayjs = require("dayjs");
+
+const dayjs = require('dayjs');
+const fs = require('fs');
+
+const nowTime = dayjs().startOf('day');
+const inputTimeJson = require('./InputTime.json');
+let inputTimeString = inputTimeJson.inputTime;
+let inputTime = dayjs(inputTimeJson.inputTime).startOf('day');
+
+if (inputTime.isSame(nowTime)){
+    console.log('Input time is now');
+}
+else if (inputTime.isBefore(nowTime)){
+    console.log('Input time is in the past')
+}
+else if (inputTime.isAfter(nowTime)){
+    console.log('Input time is in the future')
+}
+
